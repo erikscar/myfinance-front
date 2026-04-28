e3e95a9603cf6d1706cdeb663ce3ee24cb4c5a91
Erik Scarcela Araujo <earaujo@albacore.com.br>
2026-04-28 17:42:45 -0300

    feat: add images carousel and functions to handle swipe and active carousel images

---
+++ src/app/Interfaces/CustomImage.ts
@@ -0,0 +1,4 @@
+export interface CustomImage {
+    path: string,
+    alt: string,
+}
--- src/app/pages/welcome/welcome.ts
+++ src/app/pages/welcome/welcome.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { CustomImage } from '../../Interfaces/CustomImage';
+import { NgStyle } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [NgStyle, RouterLink],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss',
 })
 export class Welcome {
-  homepageImagePath: string = "chartIcon";
-
+  items: CustomImage[] = [
+    {
+      path: "assets/chart-icon.png",
+      alt: "Chart Icon"
+    },
+    {
+      path: "assets/pig-icon.png",
+      alt: "Pig Icon"
+    },
+    {
+      path: "assets/calc-icon.png",
+      alt: "Calculator Icon"
+    },
+  ]
   
+  activeIndex = 1;
+
+  onCarrouselScroll(event: Event) {
+  const element = event.target as HTMLElement;
+
+  const itemWidth = element.offsetWidth;
+  
+  const scroll = element.scrollLeft;
+  
+  this.activeIndex = Math.floor(scroll/ itemWidth) + 1;
+  }
 }
